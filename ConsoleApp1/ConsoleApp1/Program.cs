b8329d04f4b3f19af112ce499d45598582a7877b
Ramil599 <68122372+Ramil599@users.noreply.github.com>
2022-03-19 12:39:36 +0300

    Fix

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -2,11 +2,21 @@
 
 namespace ConsoleApp1
 {
+    delegate int ttt(int number);
+   
+     class Test
+    {
+      
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ttt Test = Square;
+            int res = Test(4);
+            Console.WriteLine(res);
         }
+        static int Square(int number) => number * number;
     }
 }
