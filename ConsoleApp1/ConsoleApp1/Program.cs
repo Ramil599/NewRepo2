3439ed39e203ba7db4fe4c4c02979c2a6d1d0f05
Ramil599 <68122372+Ramil599@users.noreply.github.com>
2022-03-20 14:00:35 +0300

    fin

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -2,35 +2,76 @@
 
 namespace ConsoleApp1
 {
-    
-    public delegate void OneKeyPressedHandler<TEventArgs>(char Sign);
+
+    public class OneKeyPressedEventArgs : System.EventArgs
+    {
+        public readonly char LastSign;
+        public readonly char NewSign;
+        public OneKeyPressedEventArgs(char lastSign, char newSign)
+        {
+            LastSign = lastSign; NewSign = newSign;
+        }
+        
+    }
     public class Test
     {
-        
-        public event OneKeyPressedHandler<char> OneKeyPressed;
-
+        char Sign;
+    
         public char Run()
         {
-            char Sign = Convert.ToChar(Console.ReadLine());
+             Sign = Convert.ToChar(Console.ReadLine());
             while (Sign!='c')
             {
                  Sign = Convert.ToChar(Console.ReadLine());
+                return Sign;
             }
+
             return Sign;
-            OneKeyPressed(Sign);
+
         }
-       
+
+        public event EventHandler <char>  OneKeyPressed;
+        protected virtual void OnOneKeyPressed(char OldSign, char Sign)
+        {
+            OneKeyPressed?.Invoke(this,Sign);
+        }
+        public char sign
+        {
+            get { return Sign; }
+            set
+            {
+                if (Sign == value) return;
+                char OldSign = Sign;
+                Sign = value;
+                OnOneKeyPressed(OldSign,Sign);
+            }
+        }
+
     }
+    
+    
        
     class Program
     {
         static void Main(string[] args)
         {
-
-            Test obj = new Test();
-            Console.WriteLine(obj.Run());
-            Console.ReadKey();
+            Test test = new Test();
+            test.OneKeyPressed += test_OneKeyPressed;
+            test.sign = test.Run();
         }
-        //static int Square(int number) => number * number;
+        static void test_OneKeyPressed(object sender, OneKeyPressedEventArgs e)
+        {
+            if (e.LastSign != e.NewSign)
+            {
+                Console.WriteLine($"Вы ввели новый символ");
+            }
+            else
+            {
+                Console.WriteLine(" ");
+                Console.ReadLine();
+            }
+        }
+
+
     }
 }
